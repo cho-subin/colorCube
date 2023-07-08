4305e58077250a22f186805793ecb1521f750296
whtnqls124578 <whtnqls124578@naver.com>
2023-07-08 18:15:34 +0900

    cube html+css 완료

---
+++ Cube/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className='box_container'>
+          <div className='box_300px1'>
+            <div className='box1_container'>
+              <div className='box1_300px1'></div>
+              <div className='box1_300px2'></div>
+            </div>
+            <div className='box1_container'>
+              <div className='box1_300px3'></div>
+              <div className='box1_300px4'></div>
+            </div>
+          </div>
+          <div className='box_300px2'></div>
+        </div>
+        <div className='box_container'>
+          <div className='box_300px3'>
+            <div className='box3_container'>
+              <div className='box3_300px1'></div>
+              <div className='box3_300px2'></div>
+            </div>
+            <div className='box3_container'>
+              <div className='box3_300px3'></div>
+              <div className='box3_300px4'></div>
+            </div>
+          </div>
+          <div className='box_300px4'></div>
+        </div>
+        <div className='frontBox_container1'>
+          <div className='frontBox_300px'>
+            <div className='frontBox_300px1'></div>
+            <div className='frontBox_300px2'></div>
+            <div className='frontBox_300px3'></div>
+            <div className='frontBox_300px4'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
